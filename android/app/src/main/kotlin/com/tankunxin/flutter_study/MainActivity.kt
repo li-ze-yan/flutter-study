dc66adf7164f75bfac0021f97f4c3ea8bf3e3bf7
tkx.cool <kunxin.tan@fanyou-online.com>
2025-01-08 16:41:41 +0800

    first commit

---
+++ android/app/src/main/kotlin/com/tankunxin/flutter_study/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.tankunxin.flutter_study
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/001main.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Text('hello flutter', textDirection: TextDirection.ltr, style: TextStyle(fontSize: 100, color: Colors.red),));
+}
